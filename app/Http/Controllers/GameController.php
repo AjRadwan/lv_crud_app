d47526945ceb9d7838ef1eb342450c0de5eda4dd
Anowar <1hafizajr@gmail.com>
2021-08-21 13:05:58 +0600

    Second comit

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -6,5 +6,31 @@ use Illuminate\Http\Request;
 
 class GameController extends Controller
 {
-    //
+     public function index(){
+
+     }
+
+     public function create(){
+
+     }
+
+     public function store(Request $request){
+
+     }
+
+     public function show($id){
+         
+     }
+
+     public function edit(){
+
+     }
+
+     public function update(Request $request, $id){
+
+     }
+
+     public function destroy($id){
+
+     }
 }
